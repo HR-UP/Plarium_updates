fbac59e081040d222ac4f180517945a92a8f0545
HR-UP <72601582+HR-UP@users.noreply.github.com>
2020-10-09 12:44:20 +0300

    09.10.20 12:44

---
+++ front_auth.js
@@ -0,0 +1,2 @@
+let path_host = "https://some.plarium.domain/Folder_name/"; // change this
+let path_local = "http://localhost:6660/Plarium360/";
---
+++ smtp_auth.php
@@ -0,0 +1,7 @@
+<?php
+// For mailer
+define("SMTP_HOST","https://hr-up.online/".FOLDER."/");
+define("SMTP_USERNAME","user");
+define("SMTP_PASSWORD","pass");
+define("SMTP_PORT", 465);
+define("SMTP_FROM_MAIL", "example@dot.com");
