7f1030c6b46a66862191ea406be6ae844316c32a
HR-UP <72601582+HR-UP@users.noreply.github.com>
2020-10-09 14:54:57 +0300

    Update smtp_auth.php

--- smtp_auth.php
+++ smtp_auth.php
@@ -1,6 +1,6 @@
 <?php
 // For mailer
-define("SMTP_HOST","https://hr-up.online/".FOLDER."/");
+define("SMTP_HOST","host_name");
 define("SMTP_USERNAME","user");
 define("SMTP_PASSWORD","pass");
 define("SMTP_PORT", 465);
