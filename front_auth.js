266b2b181acab631fa4222e7737c30aea4c3c446
HR-UP <72601582+HR-UP@users.noreply.github.com>
2021-09-23 09:28:56 +0300

    upd 23.09.21

--- front_auth.js
+++ front_auth.js
@@ -1,2 +1,2 @@
-let path_host = "https://hr-up.online/Plarium360/";  //"https://domain.chain/Folder_name/"; // change this
+let path_host = "http://evaluation.plarium.local/";  //"http://hr-up.online/Plarium360/";  //"https://domain.chain/Folder_name/"; // change this
 let path_local = "http://localhost:6660/Plarium360_box/";
